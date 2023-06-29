87e81d7925d07ec902e39deb346ce5ef05c80d4c
Rameshwar Singh <rameshwar.job2@gmail.com>
2023-06-29 15:57:14 -0400

    Added MaterialCommunityIcon module from expo

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import colors from "../config/colors";
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons
+          name="close"
+          color="white"
+          size={30}
+        ></MaterialCommunityIcons>
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="delete"
+          color="white"
+          size={30}
+        ></MaterialCommunityIcons>
+      </View>
       <Image
         resizeMode="contain"
         style={styles.Image}
@@ -16,17 +30,11 @@ function ViewImageScreen(props) {
 }
 const styles = StyleSheet.create({
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,

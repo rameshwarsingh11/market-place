bbd08b25010c7be2f6b8855dc778b7c56fc9b2eb
Rameshwar Singh <rameshwar.job2@gmail.com>
2023-07-09 11:19:37 -0400

    Added Custom Picker

---
+++ app/components/AppPicker.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Platform,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+  FlatList,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import defaultStyles from "../config/styles";
+import AppText from "./AppText";
+import Screen from "./Screen";
+import PickerItem from "../components/PickerItem";
+
+function AppPicker({ icon, placeholder, items }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <React.Fragment>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)}></Button>
+          <FlatList
+            data={items}
+            keyExtractor={(item) => item.value.toString()}
+            renderItem={({ item }) => (
+              <PickerItem
+                label={item.label}
+                onPress={() => console.log("item pressed::", item)}
+              />
+            )}
+          ></FlatList>
+        </Screen>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: defaultStyles.colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    passing: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
+export default AppPicker;

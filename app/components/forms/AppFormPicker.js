ae290ac6ee6a5ee9b721ed420a97a656db00e644
Rameshwar Singh <rameshwar.job2@gmail.com>
2023-07-21 13:54:44 -0400

    Bug fixing only

--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -7,15 +7,12 @@ function AppFormPicker({ items, name, placeholder }) {
   const { errors, setFieldValue, touched, values } = useFormikContext();
 
   return (
-    <>
-      <AppPicker
-        items={items}
-        onSelectItem={(item) => setFieldValue(name, item)}
-        placeholder={placeholder}
-        selectedItem={values[name]}
-      />
-      <ErrorMessage error={errors[name]} visible={touched[name]} />
-    </>
+    <AppPicker
+      items={items}
+      onSelectItem={(item) => setFieldValue(name, item)}
+      placeholder={placeholder}
+      selectedItem={values[name]}
+    />
   );
 }
 

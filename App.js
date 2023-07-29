c503196168486bb71ac506b578c947ad8c495e1a
Rameshwar Singh <rameshwar.job2@gmail.com>
2023-07-29 10:40:32 -0400

    Added device permission code for camera access

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Platform, StatusBar } from "react-native";
 
 import "react-native-gesture-handler";
-
+import * as ImagePicker from "expo-image-picker";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import ListingDetails from "./app/screens/ListingDetails";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) {
+      alert("Please enable permission to access your media library");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 }
 const containerStyle = { backgroundColor: "orange" };
 const styles = StyleSheet.create({

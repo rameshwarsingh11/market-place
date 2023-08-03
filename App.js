0fe9082e96a0d2277a9b6b1240f2b543d7b6596f
Rameshwar Singh <rameshwar.job2@gmail.com>
2023-08-03 11:40:58 -0400

    Added ImageInputList component
    -Code cleanup
    -New component added

--- App.js
+++ App.js
@@ -1,40 +1,26 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Platform, StatusBar, Button, Image } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Platform, StatusBar } from "react-native";
 
 import "react-native-gesture-handler";
-import * as ImagePicker from "expo-image-picker";
+
 import Screen from "./app/components/Screen";
-import * as Camera from "expo-camera";
-import ImageInput from "./app/components/ImageInput";
+
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState();
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) {
-      alert("Please enable permission to access your media library");
-    }
+  const [imageUris, setImageUris] = useState([]);
+  const handleAdd = (uri) => {
+    setImageUris([...imageUris, uri]);
   };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
-
-  const selectImage = async () => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.canceled) {
-        setImageUri(result.imageUri);
-      }
-    } catch (error) {
-      console.log("There was an error reading the Image", error);
-    }
+  const handleRemove = (uri) => {
+    setImageUris(imageUris.filter((imageUri) => imageUri != uri));
   };
   return (
     <Screen>
-      <ImageInput
-        imageUri={imageUri}
-        onChangeImage={(uri) => setImageUri(uri)}
+      <ImageInputList
+        imageUris={imageUris}
+        onAddImage={handleAdd}
+        onRemoveImage={handleRemove}
       />
     </Screen>
   );

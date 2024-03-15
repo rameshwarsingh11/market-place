5c050c8b033928076a12cc8d61a9d94765a0b1d9
Rameshwar Singh <rameshwar.job2@gmail.com>
2024-03-15 18:55:32 -0400

    Bug fixes

--- app/components/Card.js
+++ app/components/Card.js
@@ -7,7 +7,7 @@ import { StyleSheet, Image } from "react-native";
 function Card({ title, subTitle, imageUrl }) {
   return (
     <View style={styles.card}>
-      <Image style={styles.iamge} source={{ uri: imageUrl }} />
+      <Image style={styles.image} source={{ uri: imageUrl }} />
 
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>{title}</AppText>
